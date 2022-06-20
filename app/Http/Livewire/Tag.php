4b35b734b706c3bdd40b56f2dc36fd5ea46d9c21
shukeel <50740328+shukeel@users.noreply.github.com>
2022-06-20 14:48:16 +0300

    first commit

---
+++ app/Http/Livewire/Tag.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Tag extends Component
+{
+    public function render()
+    {
+        return view('livewire.tag')
+                ->layout('layouts.guest');
+    }
+}
