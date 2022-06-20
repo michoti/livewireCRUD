0c9415fa990e14ce8912049f647655f813c5df04
shukeel <50740328+shukeel@users.noreply.github.com>
2022-06-20 16:15:37 +0300

    modified

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Tag;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['middleware' => ['auth', 'verified'], 'prefix' => 'tags'], function(){
+    Route::get('/', Tag::class)->name('tags');
 });
 
 Route::middleware([
